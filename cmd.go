8e984d6fdf0f0637c04d7c6750831e1be203ff05
jo-mc <joe.pky@gmail.com>
2020-04-09 21:53:04 +0930

    most functioning

--- cmd.go
+++ cmd.go
@@ -51,24 +51,21 @@ func aHisto(r io.Reader, w io.Writer) error {
 	scanner := bufio.NewReader(r)
 	var depthLine []string
 	var newval int
-/* 	var chrsize uint64
+	var chrSize uint64
+	var histsize = 512
 	var histogram[512] uint32  // depth histogram for whole chromosome 1 - 511 no 0 as this will be to big
-	var maxhisto uint32
-	var regionhistogram[512] uint32 // depth histogram for each region
+	var maxhisto uint32 // region max of histo from 1 to 510
+	maxhisto = 0
+//	var regionhistogram[512] uint32 // depth histogram for each region
 	var maxregionhisto uint32
-	var regionpercenthistogram[512] uint // region % histogram
-	var regionzero[1024] uint32  // region zero run counts
-	var regionmax int32 // max depth per region, ie greater than 511
+//	var regionpercenthistogram[512] uint // region % histogram
+//	var regionzero[1024] uint32  // region zero run counts
+//	var regionmax int32 // max depth per region, ie greater than 511
 	var regionName string
 	var lastRegion string
-	lastRegion = "_last_region" */
-	// for scanner.Scan() {
-	// 	_, e := fmt.Fprintln(
-	// 		w, strings.ToUpper(scanner.Text()))
-	// 	if e != nil {
-	// 		return e
-	// 	}
-	// }
+	lastRegion = "_last_region" 
+
+	
 	for {
 		// text expected "chr1  1   1" region, position , readdepth
 		// (if -a specified in samtools depth the position will be 0- length of region) otherwise 0 depths are omitted from output)
@@ -79,28 +76,64 @@ func aHisto(r io.Reader, w io.Writer) error {
 			fmt.Println("finshed reading input")
 			break
 		}		
-		// get current value to add to histogram field 0 or 3
+		// get file line current value to add to histogram field 0 or 3
 		depthLine = strings.Fields(line)
 		switch {
 		case len(depthLine) == 1:
 			newval, err = strconv.Atoi(depthLine[0])
+			regionName = "file"
+			lastRegion = regionName
 		case len(depthLine) == 3:	
 			newval, err = strconv.Atoi(depthLine[2])
+			regionName = depthLine[0]
+			if lastRegion == "_last_region" {
+				lastRegion = regionName
+			}
 		case len(depthLine)	== 0:
 			continue  // empty line, go read next line ?
 		default	:
 			newval = -1
 		}
 		if err != nil {
-			fmt.Println(" data non numeric, exiting")	
+			fmt.Println(" data non numeric/integer, exiting")	
 			os.Exit(-1)	
 		}
+		// end get data from file line
+		// now test if we have new region
+		if regionName != lastRegion {
+			for i := 1; i < 511; i++ {
+				if histogram[i] > maxhisto {
+					maxhisto = histogram[i]
+				}
+			}
+			printRegion(lastRegion,histogram[:],chrSize, maxhisto, maxregionhisto)
+			maxhisto = 0
+			maxregionhisto = 0
+		}
+		// end new region test 
+		//fmt.Println(newval)
+		// update histogram
+		chrSize = chrSize + 1  // count of data in region
+		if uint32(newval) > maxregionhisto {
+			maxregionhisto = uint32(newval)
+		}
+		if newval > histsize - 1 {
+			histogram[histsize-1] = histogram[histsize-1] + 1
+		} else {
+			histogram[newval] = histogram[newval] + 1
+		}
 
-		fmt.Println(newval)
+		// end updat histogram
 		// fmt.Println(depthLine[1])
 		// fmt.Println(depthLine[2])
-
+		lastRegion = regionName
 		}
+		for i := 1; i < 511; i++ {
+			if histogram[i] > maxhisto {
+				maxhisto = histogram[i]
+			}	
+		}		
+		printRegion(lastRegion,histogram[:],chrSize,maxhisto,maxregionhisto)
 	return nil
 }
 
@@ -110,4 +143,17 @@ func fileExists(filepath string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func printRegion(region string, histogram []uint32, chrsize uint64, maxhisto uint32, maxregionhisto uint32  ) {
+
+	fmt.Println("first 8 and last values of histogram:",region)
+	for i := 0; i < 8; i++ {
+		fmt.Println("histogram[",i,"] : ",histogram[i])
+	}
+	fmt.Println("histogram[511] : ",histogram[511])
+	fmt.Println("Number of data read: ",chrsize)
+	fmt.Println("Maximum value 1-510: ",maxhisto)
+	fmt.Println("Maximum value : ",maxregionhisto)
+	fmt.Println("Zero count : ",histogram[0])
 }
